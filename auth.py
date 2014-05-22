eea1fd433afcd458aa85d7eed2c5e0be3bd6c4bd
Victor <theonlypwner@users.noreply.github.com>
2014-05-21 19:10:59 -0600

    Use a script to log in

---
+++ auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import getpass
+import urllib
+import urllib2
+
+print('Enter the username of the reddit account.')
+username = raw_input('--> ')
+
+print('Enter the password of the reddit account.')
+password = getpass.getpass('--> ')
+
+payload = urllib.urlencode({
+    'api_type': 'json',
+    'user': username,
+    'passwd': password,
+})
+req = urllib2.Request('https://ssl.reddit.com/api/login', payload)
+req.add_header('User-Agent', 'reddit-grammar-bot login script')
+response = urllib2.urlopen(req)
+print(response.read())
